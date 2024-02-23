f2a221601ba78cb4b24404b239cce668b6964538
Dimitrios Mouratidis <76875895+dimitrmo@users.noreply.github.com>
2024-02-23 13:56:06 +0000

    Add output

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 // const github = require('@actions/github');
 
-try {
-  // version
-  const version = core.getInput('version');
-  console.log(`Version: ${version}!`);
-  // message
-  const message = core.getInput('message');
-  console.log(`Message: ${message}`);
-  // git-tag-version
-  const git_tag_version = core.getInput('git-tag-version');
-  console.log(`Git tag version: ${git_tag_version}`);
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+async function run() {
+  try {
+    // version
+    const version = core.getInput('version');
+    console.log(`Version: ${version}!`);
 
-  let command = `cargo verison ${version} -m "${message}"`;
-  if (git_tag_version === 'false' || git_tag_version === false) {
-    command += ' --git-tag-version=false'
-  }
+    // message
+    const message = core.getInput('message');
+    console.log(`Message: ${message}`);
 
-  console.log(`I will run command: ${command}`);
-} catch (error) {
-  core.setFailed(error.message);
+    // git-tag-version
+    const git_tag_version = core.getInput('git-tag-version');
+    console.log(`Git tag version: ${git_tag_version}`);
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  
+    let command = `cargo verison ${version} -m "${message}"`;
+    if (git_tag_version === 'false' || git_tag_version === false) {
+      command += ' --git-tag-version=false'
+    }
+  
+    const output = await exec.getExecOutput('ls', ['-lah'], { silent: true })
+    let new_version = output.stdout.trim();
+    core.setOutput("new_version", new_version);
+  
+    console.log(`I will run command: ${command}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }  
 }
+
+run();
